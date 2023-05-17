86abb9d067f48aac36f66319b26b36d6c1b787dc
NTSinhDev <ntsinh.dev@gmail.com>
2023-05-17 07:46:03 +0700

    initial source

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="sinh" style={{
+        display: 'flex',
+        flexDirection: 'column', alignItems: 'center'
+      }}>
+        <a href="#"><h4>NTSinh.Dev@gmail.com</h4></a>
+        <h1>Success</h1>
+      </div>
+    </div >
+  );
+}
+
+export default App;
